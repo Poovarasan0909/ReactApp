0ddfa7ffb4fdcbcf7b787b33781d4e1236986680
Poovarasan <poovarasanm0909@gmail.com>
2024-01-07 23:06:59 +0530

    testing porpose

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
+import './css/contentPage.css';
+import './css/background.css';
 //import Sample from "./sample";
 import IssuePage from './IssuePage';
+// import KeyValuePairs from "./table/keyValueTable";
+import InputWithOptions from "./InputWithOptions";
+import Gemini_api from "./AI/Gemini_api";
 
 function App() {
   return (
   <div>
     <div className="App">
-         <IssuePage />
+         {/*<IssuePage />*/}
+        {/*<KeyValuePairs />*/}
+        {/*<InputWithOptions/>*/}
+        <Gemini_api/>
     </div><br/><br/>
-     <footer><p><small><u style={{color:"grey"}}>created by </u></small>Poovarasan</p></footer>
+     <footer><p><small><u style={{color:"grey"}}>created by </u></small></p></footer>
     </div>
   );
 }
--- src/IssuePage.jsx
+++ src/IssuePage.jsx
@@ -8,16 +8,65 @@ class IssuePage extends Component {
     constructor() {
         super();
         this.TableStore = new TableStore();
+        this.state = {
+            count : 0
+        }
     }
 
-    render() {
-        const {csvData } = this.TableStore;
+    componentDidMount() {
+        // this.countf();
+        // setInterval(() => {
+        //     console.log("This executes repeatedly at the specified interval.");
+        // }, 5000);
+    }
+    //
+    countf = () => {
+        const loop = [1,2,3,4,5,6]
+        loop.map((value,index) =>{
+            if(value !== 4)
+            console.log(index, "--> index")
+        } )
 
+        // console.log("11111111111")
+        // for(let i=0; i``````` <= 5; i++) {
+        //     setTimeout(() => {
+        //         console.log(i*1000)
+        //         // this.setState({count: i})
+        //             document.getElementById("countid").innerText = i
+        //         }, i*1000);
+        // }
+    }
+
+
+    // countf = () => {
+    //     console.log("11111111111");
+    //     let i = 0;
+    //     const updateCount = () => {
+    //         if (i <= 10) {
+    //             console.log("mppppppp");
+    //             this.setState({ count: i });
+    //             document.getElementById("countid").innerText = i;
+    //             i++;
+    //             setTimeout(updateCount, 1000);
+    //         }
+    //     };
+    //
+    //     updateCount();
+    // }
+
+
+    render() {
+        this.countf();
+        const {csvData } = this.TableStore;
         return (
             <div>
-                <h1>Issue Page</h1><br />
-                <CsvToTable csvData={csvData} />
-                <ExitingCSVWrite />
+                {/*<h1>Issue Page</h1><br />*/}
+                {/*<CsvToTable csvData={csvData} />*/}
+                {/*<ExitingCSVWrite />*/}
+
+                <h1>testing</h1>
+                <div id="countid">0</div>
+
             </div>
         );
     }
